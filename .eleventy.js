07c842cc661851669452ef1ede92c05da74a9083
jaykumarpatel2710@gmail.com <jaykumarpatel2710@gmail.com>
2021-10-02 17:56:15 +0530

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addFilter("postDate", (dateObject) => {
         return DateTime.fromJSDate(dateObject).toLocaleString(DateTime.DATE_MED)
     })
